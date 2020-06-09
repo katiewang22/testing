a22ffa97f2e10a427fcef08a6d1424b7a3c3fb38
Katie Wang <katie.wang1950f@gmail.com>
2020-06-09 13:59:13 -0400

    Initial Commit

---
+++ testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by Quan Wang on 6/9/20.
+//  Copyright © 2020 Katie Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
